d3269d0a2887ade45b2580a2ecaba066444365fa
Nanpane Adigono Yann-Mahel <yann.nanpane@2028.ucac-icam.com>
2024-10-24 11:24:40 +0100

    Backend

---
+++ routes/apiR.php
@@ -0,0 +1,15 @@
+<?php
+//
+//use App\Http\Controllers\UtilisateurController;
+//use App\Http\Controllers\EmployeController;
+//use Illuminate\Support\Facades\Route;
+//
+//
+//Route::get('/utilisateurs', [utilisateurController::class, 'index']);
+//Route::post('/utilisateurs', [utilisateurController::class, 'store']);
+//Route::delete('/utilisateurs/{id_utilisateur}', [utilisateurController::class, 'destroy']);
+//
+//Route::get('/employe', [EmployeController::class, 'index']);
+//Route::post('/employe', [EmployeController::class, 'store']);
+//Route::delete('/employe/{id_employe}', [EmployeController::class, 'destroy']);
+//
